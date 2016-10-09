36603ab8e34699c67cd913f8a4e21d603f08935c
Hector Garcia <soseinhg3@gmail.com>
2016-10-09 04:30:01 +0000

    Revert to using ctx.url and refactor originServer

--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,19 @@ import { generate as randomstring } from 'randomstring';
 
 const router = new Router();
 
-let originServer;
+let origin = {};
 
 // @stephenhay's url validation regex via
 // https://mathiasbynens.be/demo/url-regex
-router.get('\/(https?):\/\/([^\s/$.?#].[^\s]*)', async (ctx) => {
-  originServer = originServer || {
-    href: `https://${ctx.hostname}${ctx.get('x-forwarded-path')}`,
-    reHost: new RegExp(`^${ctx.hostname}.+$`)
-  };
-  const url = ctx.captures.join('');
+router.get(/^\/https?:\/\/[^\s/$.?#].[^\s]*$/, async (ctx) => {
+  if (!origin.href) {
+    origin.href = `${ctx.hostname}${ctx.get('x-forwarded-path')}\/`;
+    origin.regex = new RegExp(`^https?:\/\/${origin.href}.+$`);
+  }
 
-  if (originServer.reHost.test(ctx.captures[1])) ctx.throw(403);
+  const url = ctx.url.slice(1);
+
+  if (origin.regex.test(url)) ctx.throw(403);
 
   const aliases = ctx.db.collection('aliases');
   const document = await aliases.findOne({url});
@@ -24,7 +25,7 @@ router.get('\/(https?):\/\/([^\s/$.?#].[^\s]*)', async (ctx) => {
 
   if (document === null) await aliases.insert({alias, url});
 
-  ctx.body = {'shortened_url': `${originServer.href}/${alias}`};
+  ctx.body = {'shortened_url': `https://${origin.href}${alias}`};
   ctx.type = 'json';
 });
 
